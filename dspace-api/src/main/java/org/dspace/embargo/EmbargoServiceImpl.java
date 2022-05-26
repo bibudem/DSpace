90d5546420056647332b63d2498ec7e75febd2bf
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2022-05-26 11:04:05 -0400

    Fonction maison utilisée pour convertir une période d'embargo ( 6 mois, 12 mois etc.) dans une date.

--- dspace-api/src/main/java/org/dspace/embargo/EmbargoServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/embargo/EmbargoServiceImpl.java
@@ -26,6 +26,15 @@ import org.dspace.embargo.service.EmbargoService;
 import org.dspace.services.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+// add UdeM 2022
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Calendar;
+
+
 /**
  * Public interface to the embargo subsystem.
  * <p>
@@ -93,7 +102,9 @@ public class EmbargoServiceImpl implements EmbargoService {
                 return;
             }
         }
+
         String slift = myLift.toString();
+        
         try {
             context.turnOffAuthorisationSystem();
             itemService.clearMetadata(context, item, lift_schema, lift_element, lift_qualifier, Item.ANY);
@@ -113,8 +124,8 @@ public class EmbargoServiceImpl implements EmbargoService {
         throws SQLException, AuthorizeException {
         List<MetadataValue> terms = itemService.getMetadata(item, terms_schema, terms_element,
                                                             terms_qualifier, Item.ANY);
-
         DCDate result = null;
+
 
         // Its poor form to blindly use an object that could be null...
         if (terms == null) {
@@ -127,9 +138,11 @@ public class EmbargoServiceImpl implements EmbargoService {
         if (result == null) {
             return null;
         }
+       
 
         // new DCDate(non-date String) means toDate() will return null
         Date liftDate = result.toDate();
+        
         if (liftDate == null) {
             throw new IllegalArgumentException(
                 "Embargo lift date is uninterpretable:  "
@@ -140,6 +153,17 @@ public class EmbargoServiceImpl implements EmbargoService {
          * NOTE: We do not check here for past dates as it can result in errors during AIP restoration.
          * Therefore, UIs should perform any such date validation on input. See DS-3348
          */
+        //UdeM 2022 | ajouter la periode en mois a la date d'aujourd'hui
+        List<MetadataValue> issued = itemService.getMetadata(item,MetadataSchemaEnum.DC.getName(), "date", "issued", Item.ANY);
+
+        String valChamp = terms.get(0).getValue().toString();
+        String valIssued = issued.get(0).getValue().toString();
+
+        //log.info("Champ: " + valChamp);
+        //log.info("Issued: " + valIssued);
+
+        result = calculerPeriodeEmbargoUdeM(valChamp,valIssued);
+
         return result;
     }
 
@@ -179,6 +203,7 @@ public class EmbargoServiceImpl implements EmbargoService {
                     "Missing one or more of the required DSpace configuration properties for EmbargoManager, check " +
                         "your configuration file.");
             }
+           
             terms_schema = getSchemaOf(terms);
             terms_element = getElementOf(terms);
             terms_qualifier = getQualifierOf(terms);
@@ -229,6 +254,63 @@ public class EmbargoServiceImpl implements EmbargoService {
         }
         return liftDate;
     }
+    
+     /* add UdeM 2022 | Calculer le Left embargo a aprtir du dc.date.issued et selon le choix dans la periode Term */   
+    protected  DCDate calculerPeriodeEmbargoUdeM(String valueTermFild, String valuedIssued)  {
+
+        // verifier si la date issued est non vide
+        if(valuedIssued == null){
+            log.info("La date issued ne doit pas etre vide:  " + valuedIssued);
+            return null;
+        } 
+
+        DCDate liftDate = null;
+
+        Calendar cal = Calendar.getInstance();
+
+        // verifier si la date issued est bien complet yyyy-MM-dd sinon on complet avec 1 janvier d'année recoupéré
+        String[] parts = valuedIssued.split("-");
+         
+        // Si le format de la date est correct le result de valueTermFild.indexOf("-") = 4, dans le cas contraire on return null
+        /*if(valueTermFild.indexOf("-") != 4){
+            log.info("La date issued n'a pas un bon format:  " + valuedIssued);
+            return null;
+        }*/
+        
+        // si le mois et le jour n'est pas indiqué
+        if(parts.length == 1) {
+          valuedIssued += "-01-01";
+        }
+        // si le jour n'est pas indiqué
+        if(parts.length == 2) {
+          valuedIssued += "-01";
+        }
+
+        log.info("Date create: " + valuedIssued);
+
+        //Convertir string to date
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        formatter.setTimeZone(TimeZone.getDefault());
+
+       try {
+            Date dateConvert = formatter.parse(valuedIssued);
+            // creation du calendrier a partir de cette date
+            cal.setTime(dateConvert);
+            // on ajout la periode
+            cal.add(Calendar.MONTH, Integer.parseInt(valueTermFild));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+
+        //log.info("Date convert: " + cal.getTime());  
+
+        liftDate = new DCDate(cal.getTime());     
+
+        return liftDate;
+    }
 
     @Override
     public void checkEmbargo(Context context, Item item) throws SQLException, IOException, AuthorizeException {
@@ -245,4 +327,8 @@ public class EmbargoServiceImpl implements EmbargoService {
         throws SQLException, IOException, AuthorizeException {
         return itemService.findByMetadataField(context, lift_schema, lift_element, lift_qualifier, Item.ANY);
     }
+
+
+
+  
 }
